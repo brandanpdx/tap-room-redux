e8fce0f8ce3d70155c6a7d6ea62409a9198cc88f
Brandan Sayarath <brandan@brandan.tech>
2020-04-24 10:18:56 -0700

    add NewKegForm.js

---
+++ src/NewKegForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NewKegForm(props) {
+  return (
+    <React.Fragment>
+      <form>
+        <h3>Form Placeholder</h3>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default NewKegForm; 

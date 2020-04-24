baeb3246669e08fec200a6f1f7dea7c5f4d3df38
Brandan Sayarath <brandan@brandan.tech>
2020-04-24 15:42:48 -0700

    update KegDetail button and props

--- src/KegDetail.js
+++ src/KegDetail.js
@@ -14,13 +14,15 @@ function KegDetail(props) {
       <p>{keg.ABV}</p>
       <p>{keg.quantity}</p>
       <button onClick={() => onClickingDelete(keg.id)}>Remove Keg</button>
+      <button onClick={props.onClickingEdit}>Update Keg Details</button>
     </React.Fragment>
   )
 }
 
 KegDetail.propTypes = {
   keg: PropTypes.object,
-  onClickingDelete: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 };
 
 export default KegDetail; 

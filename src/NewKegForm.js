8d2c12cacf03fde3b7f55a4f3cc1ee581b36713d
Brandan Sayarath <brandan@brandan.tech>
2020-04-24 11:12:03 -0700

    update src/NewItemForm.js to include event handler function

--- src/NewKegForm.js
+++ src/NewKegForm.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import { v4 } from 'uuid';
 
+
+function handleNewKegFormSubmission(event) {
+  event.preventDefault();
+  console.log(event.target.name.value);
+  console.log(event.target.description.value);
+  console.log(event.target.quantity.value);
+}
+
 function NewKegForm() {
   return (
     <React.Fragment>
-      <form onSubmit={handleNewItemFormSubmission}>
+      <form onSubmit={handleNewKegFormSubmission}>
         <input
           type='text'
           name='name'
-          placeholder='item name' />
+          placeholder='Beer Name' />
         <input
           type='text'
-          name='description'
-          placeholder='item description' />
+          name='brand'
+          placeholder='Beer Brand' />
+        <input
+          type='number'
+          name='price'
+          placeholder='Beer Price' />
+        <input
+          type='number'
+          name='ABV'
+          placeholder='ABV' />
         <input
           type='number'
           name='quantity'
-          placeholder='initial quatity' />
-        <button type='submit'>add item</button>
+          placeholder='Quantity (Pints)' />
+        <button type='submit'>Add Keg</button>
       </form>
     </React.Fragment>
   );
 }
+
+
 
 export default NewKegForm; 
 

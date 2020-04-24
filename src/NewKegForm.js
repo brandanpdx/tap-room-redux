a99f62fb2b4ff53d6bcb6ca7d23f5caaedc380ca
Brandan Sayarath <brandan@brandan.tech>
2020-04-24 11:05:49 -0700

    update src/NewItemForm.js to include a form with a submit event handler

--- src/NewKegForm.js
+++ src/NewKegForm.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { v4 } from 'uuid';
 
-function NewKegForm(props) {
+function NewKegForm() {
   return (
     <React.Fragment>
-      <form>
-        <h3>Form Placeholder</h3>
+      <form onSubmit={handleNewItemFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='item name' />
+        <input
+          type='text'
+          name='description'
+          placeholder='item description' />
+        <input
+          type='number'
+          name='quantity'
+          placeholder='initial quatity' />
+        <button type='submit'>add item</button>
       </form>
     </React.Fragment>
   );
 }
 
 export default NewKegForm; 
+

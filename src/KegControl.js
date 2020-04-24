9ea158d1ed69949c71b8d90faf229b8d21d81ff4
Brandan Sayarath <brandan@brandan.tech>
2020-04-24 13:52:58 -0700

    update KegControl to render KegDetail

--- src/KegControl.js
+++ src/KegControl.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NewKegForm from './NewKegForm.js';
 import KegList from './KegList.js';
+import KegDetail from './KegDetail';
 
 class KegControl extends React.Component {
 
@@ -8,8 +9,14 @@ class KegControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      masterKegList: []
+      masterKegList: [],
+      selectedKeg: null
     };
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.masterKegList.filter(keg => keg.id === id)[0];
+    this.setState({ selectedKeg: selectedKeg });
   }
 
   handleAddingNewKegToList = (newKeg) => {
@@ -29,11 +36,16 @@ class KegControl extends React.Component {
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+    if (this.state.selectedKeg != null) {
+      currentlyVisibleState = <KegDetail keg={this.state.selectedKeg} />
+      buttonText = "return to kegs";
+    } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />
       buttonText = "Return to Keg List";
     } else {
-      currentlyVisibleState = <KegList kegList={this.state.masterKegList} />
+      currentlyVisibleState = <KegList
+        kegList={this.state.masterKegList}
+        onKegSelection={this.handleChangingSelectedKeg} />
       buttonText = "Add new Keg";
     }
 

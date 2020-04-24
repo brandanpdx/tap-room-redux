4adbbf5c2f41a6e6659ffdf2a1f2f395ce0c7de0
Brandan Sayarath <brandan@brandan.tech>
2020-04-24 12:22:18 -0700

    add method to handle form submission

--- src/NewKegForm.js
+++ src/NewKegForm.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { v4 } from 'uuid';
+import PropTypes from 'prop-types'; 
+
+function NewKegForm(props) {
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      price: event.target.price.value,
+      ABV: event.target.ABV.value,
+      quantity: event.target.quantity.value,
+      id: v4()
+    });
+  }
 
 
-function handleNewKegFormSubmission(event) {
-  event.preventDefault();
-  console.log(event.target.name.value);
-  console.log(event.target.brand.value);
-  console.log(event.target.price.value);
-  console.log(event.target.ABV.value);
-  console.log(event.target.quantity.value);
-}
-
-function NewKegForm() {
   return (
     <React.Fragment>
       <form onSubmit={handleNewKegFormSubmission}>
@@ -42,6 +47,9 @@ function NewKegForm() {
 }
 
 
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+}
 
 export default NewKegForm; 
 

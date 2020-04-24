b8fdeed6d30bb283de031a4763f76a4b654503d4
Brandan Sayarath <brandan@brandan.tech>
2020-04-24 11:00:56 -0700

    update src/ItemControl.js to include buttons to toggle between New Keg and return to Kegs

--- src/KegControl.js
+++ src/KegControl.js
@@ -11,21 +11,30 @@ class KegControl extends React.Component {
     };
   }
 
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+
   render() {
     let currentlyVisibleState = null;
+    let buttonText = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm />
+      buttonText = "Return to Keg List";
     } else {
       currentlyVisibleState = <KegList />
+      buttonText = "Add new Keg";
     }
 
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick ={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
 }
-
 
 export default KegControl; 

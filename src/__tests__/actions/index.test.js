930d945bf352b81677616f7ebc655f72d4cf4911
Brandan Sayarath <brandan@brandan.tech>
2020-05-01 15:13:22 -0700

    pass test that creates add_keg action

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,10 +1,30 @@
 import * as actions from '../../actions';
 
 describe('keg list actions', () => {
+
   it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
       type: 'DELETE_KEG',
       id: 1
     });
   });
+
+  it('toggleFrom should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+
+  it('addKeg should create ADD_KEG action', () => {
+    expect(actions.addKeg({ name: 'Beer', brand: 'Beer Brand', price: 5, ABV: 5, quantity: 124, id: 1 })).toEqual({
+      type: 'ADD_KEG',
+      name: 'Beer',
+      brand: 'Beer Brand',
+      price: 5,
+      ABV: 5,
+      quantity: 124,
+      id: 1
+    });
+  });
+
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,20 @@ export const deleteKeg = id => ({
   type: 'DELETE_KEG',
   id
 });
+
+export const toggleForm = () => ({
+  type: 'TOGGLE_FORM'
+});
+
+export const addKeg = (keg) => {
+  const { name, brand, price, ABV, quantity, id } = keg;
+  return {
+    type: 'ADD_KEG',
+    name: name,
+    brand: brand,
+    price: price,
+    ABV: ABV,
+    quantity: quantity,
+    id: id
+  }
+}
